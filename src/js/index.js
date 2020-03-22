24e9537a8e2c30a159662a7cb62f07b4fce400c9
Fabricio Garcia <gael.fabricio@gmail.com>
2020-03-22 13:05:59 -0600

    feat: Add local storage capabilities
    - Create localStorage.js to handle user requests
    - Modify index.js accordingly
    
    Refs. #2

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,13 @@
 import Weather from './weather';
 import Dom from './dom';
+import LocalStorage from './localStorage';
 
 require('../css/style.css');
 
 const dom = new Dom();
-const weather = new Weather('London', 'uk');
+const localStorage = new LocalStorage();
+const { city, countryCode } = localStorage.getLocationData();
+const weather = new Weather(city, countryCode);
 
 async function fetchWeather() {
   const data = await weather.getWeather();
@@ -16,6 +19,7 @@ document.getElementById('search-location').addEventListener('click', (evt) => {
   const city = document.getElementById('city').value;
   const countryCode = document.getElementById('country-code').value;
   weather.searchLocation(city, countryCode);
+  localStorage.setLocationData(city, countryCode);
   fetchWeather();
 });
 
