8dba8879f32c4e988aa6700c08af254a9ccfbc5c
Fabricio Garcia <gael.fabricio@gmail.com>
2020-03-21 18:34:20 -0600

    feat: DOM manipulation
    - Add dom.js to properly manage and manipulate  data from the DOM
    
    Refs. #4 and also #2

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,15 @@
 import Weather from './weather';
+import Dom from './dom';
 
 require('../css/style.css');
 
+const dom = new Dom();
 const weather = new Weather('London', 'uk');
 
 async function fetchWeather() {
   const data = await weather.getWeather();
   console.log(data);
+  dom.render(data);
 }
 
 document.addEventListener('DOMContentLoaded', fetchWeather);
